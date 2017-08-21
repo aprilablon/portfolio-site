2a58b419d7785293a59e767ed5e4a15cfcde68e2
April Ablon <ablonapril@gmail.com>
2017-08-21 15:58:42 -0700

    Change static text to react-router-dom Link components for navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Header.css';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="row header">
-        <div className="col-xs-12 col-sm-3" id="home">April Ablon</div>
+        <Link to="/" className="col-xs-12 col-sm-3 link" id="home">April Ablon</Link>
         <div className="col-xs-12 col-sm-9" id="nav">
-          <div className="nav-link">Projects</div>
-          <div className="nav-link">About</div>
-          <div className="nav-link">Contact</div>
+          <Link to="/" className="nav-link link">Projects</Link>
+          <Link to="/about" className="nav-link link">About</Link>
+          <Link to="/contact" className="nav-link link">Contact</Link>
         </div>
       </div>
     );
