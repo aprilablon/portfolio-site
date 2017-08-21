ce58f33dca7035c3eb46c11ed07360d8c2b0cbf1
April Ablon <ablonapril@gmail.com>
2017-08-21 15:58:01 -0700

    Add react router functionality to navigate to other pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
 import Header from './Header';
 import Projects from './Projects';
+import About from './About';
+import Contact from './Contact';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Projects />
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={Projects}/>
+            <Route path="/About" component={About} />
+            <Route path="/Contact" component={Contact} />
+          </div>
+        </Router>
       </div>
     );
   }
