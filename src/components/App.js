d44d12f761627435513614902726af340ff987fa
April Ablon <ablonapril@gmail.com>
2017-08-23 16:27:06 -0700

    Remove unnecessary imports from react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './Header';
 import Projects from './Projects';
 import About from './About';
