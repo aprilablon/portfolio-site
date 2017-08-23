84b4622d89d6726c515d6e19c0fe3e976a258369
April Ablon <ablonapril@gmail.com>
2017-08-23 16:27:58 -0700

    Add alt field to img tag

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,18 +7,18 @@ class Projects extends React.Component {
       <div id="projects">
         <div className="row">
           <div className="col-xs-12 col-sm-6 project-panel">
-            <img src="/assets/bandwith.png" />
+            <img alt="bandwith-app" src="/assets/bandwith.png" />
           </div>
           <div className="col-xs-12 col-sm-6 project-panel">
-            <img src="/assets/fridgr.png" />
+            <img alt="fridgr-app" src="/assets/fridgr.png" />
           </div>
         </div>
         <div className="row">
           <div className="col-xs-12 col-sm-6 project-panel">
-            <img src="/assets/rocket.png" />
+            <img alt="rocket-app" src="/assets/rocket.png" />
           </div>
           <div className="col-xs-12 col-sm-6 project-panel">
-            <img src="/assets/tribal.png" />
+            <img alt="tribal-app" src="/assets/tribal.png" />
           </div>
         </div>
       </div>
