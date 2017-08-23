0f6bc9e885be40deee2ebaa38a4b43ee7cd0d7b1
April Ablon <ablonapril@gmail.com>
2017-08-23 16:27:34 -0700

    Add component state to store email message info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      toEmail: '',
+      emailMessage: '',
+      emailSubject: ''
+    };
+  }
+
+  sendEmail() {
+    
+  }
+
   render() {
     return (
       <div>
