f3b1dbbfae55f4bd30eafab87dc4a738de6ee903
April Ablon <ablonapril@gmail.com>
2017-08-23 20:45:26 -0700

    Add img components for social media icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Contact.css';
 
 class Contact extends React.Component {
   constructor(props) {
@@ -17,8 +18,12 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <div>
-        Contact Me!
+      <div id="contact">
+        <div className="row social">
+          <img className="icon" alt="linkedin.com/in/aprilablon" src="/assets/linkedin.svg" />
+          <img className="icon" alt="github.com/aprilablon" src="/assets/github.png" />
+          <img className="icon" alt="@aprilablon" src="/assets/twitter.png"/>
+        </div>
       </div>
     );
   }
