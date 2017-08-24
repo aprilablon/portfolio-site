bdb516720c2d7c6155833367996d595fd3aeb344
April Ablon <ablonapril@gmail.com>
2017-08-23 17:01:49 -0700

    Update app images displayed on home page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,18 +7,18 @@ class Projects extends React.Component {
       <div id="projects">
         <div className="row">
           <div className="col-xs-12 col-sm-6 project-panel">
-            <img alt="bandwith-app" src="/assets/bandwith.png" />
+            <img alt="bandwith-app" src="/assets/bandwith-mocks/bandwithmock_home.jpeg" />
           </div>
           <div className="col-xs-12 col-sm-6 project-panel">
-            <img alt="fridgr-app" src="/assets/fridgr.png" />
+            <img alt="fridgr-app" src="/assets/fridgr-mocks/fridgrmock_inventory1.jpeg" />
           </div>
         </div>
         <div className="row">
           <div className="col-xs-12 col-sm-6 project-panel">
-            <img alt="rocket-app" src="/assets/rocket.png" />
+            <img alt="tribal-app" src="/assets/tribal-mocks/tribalmock_play1.jpeg" />
           </div>
           <div className="col-xs-12 col-sm-6 project-panel">
-            <img alt="tribal-app" src="/assets/tribal.png" />
+            <img alt="rocket-app" src="/assets/rocket-mocks/rocketmock_past.jpeg" />
           </div>
         </div>
       </div>
