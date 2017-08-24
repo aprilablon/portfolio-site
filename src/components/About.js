ecac4bffdc1c04f112a9298a5941cf1f04afab14
April Ablon <ablonapril@gmail.com>
2017-08-23 20:44:59 -0700

    Add flexbox components and dummy text to test fonts

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../styles/About.css';
 
 class About extends React.Component {
   render() {
     return (
-      <div>
-        About Me!
+      <div id="about">
+        <div className="row">
+          <div className="col-xs-12 col-sm-8 col-sm-offset-2">
+            Hi! I'm April. An aerospace engineer turned full-stack software engineer. 
+          </div>
+        </div>
+        <div className="row">
+          JavaScript
+        </div>
+        <div className="row">
+          Skills
+        </div>
       </div>
     );
   }
