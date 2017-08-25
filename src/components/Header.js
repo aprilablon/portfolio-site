e960f1455289c6967858426d5b964b2ff290f8ba
April Ablon <ablonapril@gmail.com>
2017-08-24 17:19:30 -0700

    Add position title to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,19 @@ import '../styles/Header.css';
 class Header extends React.Component {
   render() {
     return (
-      <div className="row header">
-        <Link to="/" className="col-xs-12 col-sm-3 link" id="home">April Ablon</Link>
-        <div className="col-xs-12 col-sm-9" id="nav">
-          <Link to="/" className="nav-link link">Projects</Link>
-          <Link to="/about" className="nav-link link">About</Link>
-          <Link to="/contact" className="nav-link link">Contact</Link>
+      <div className="header">
+        <div className="row">
+          <div className="small-center">
+            <Link to="/" className="col-xs-12 col-sm-3 link" id="home">April Ablon</Link>
+            <div>
+              <p id="title">Full Stack Software Engineer</p>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-3 col-sm-offset-6 small-center" id="nav">
+            <Link to="/" className="nav-link link">Projects</Link>
+            <Link to="/about" className="nav-link link">About</Link>
+            <Link to="/contact" className="nav-link link">Contact</Link>
+          </div>
         </div>
       </div>
     );
