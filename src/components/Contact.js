3c10721e97fa884e4f7ef77fb6ca497f8208a75e
April Ablon <ablonapril@gmail.com>
2017-08-24 17:19:11 -0700

    Add helper function to update state for every input box

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,81 @@
 import React from 'react';
+// import { Email } from 'email';
 import '../styles/Contact.css';
 
 class Contact extends React.Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {
-      toEmail: '',
-      emailMessage: '',
-      emailSubject: ''
+      first: '',
+      last: '',
+      email: '',
+      subject: '',
+      message: ''
     };
+
+    this.sendEmail = this.sendEmail.bind(this);
+    this.registerInput = this.registerInput.bind(this);
+  }
+  
+  sendEmail() {
+    console.log('button click: ', this.state);
+    // let message = new Email({
+    //   from: "ablonapril@gmail.com",
+    //   to:   "ablonapril@gmail.com",
+    //   subject: "Knock knock...",
+    //   body: "Who's there?"
+    // });
+    
+    // message.send((err) => {console.log('Email did not send: ', err)});
   }
 
-  sendEmail() {
-    
+  registerInput(event) {
+    let updateObj = {};
+    updateObj[event.target.name] = event.target.value;
+    this.setState(updateObj);
   }
 
   render() {
     return (
       <div id="contact">
-        <div className="row social">
-          <img className="icon" alt="linkedin.com/in/aprilablon" src="/assets/linkedin.svg" />
-          <img className="icon" alt="github.com/aprilablon" src="/assets/github.png" />
-          <img className="icon" alt="@aprilablon" src="/assets/twitter.png"/>
+        <p className="contact-title">Say Hi 👋</p>
+        <div className="row connect-row">
+          <p className="subtitle">Connect: </p>
+          <a target="_blank" href="https://www.linkedin.com/in/aprilablon">
+            <img className="icon" alt="linkedin.com/in/aprilablon" src="/assets/linkedin.svg" />
+          </a>
+          <a target="_blank" href="https://twitter.com/aprilablon">
+            <img className="icon" alt="@aprilablon" src="/assets/twitter.png"/>
+          </a>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-3 col-sm-offset-3">
+            <input type="text" placeholder="First" name="first" onChange={(event) => this.registerInput(event)} />
+          </div>
+          <div className="col-xs-12 col-sm-3">
+            <input type="text" placeholder="Last" name="last" onChange={(event) => this.registerInput(event)} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-sm-offset-3">
+            <input type="text" placeholder="Email" name="email" onChange={(event) => this.registerInput(event)} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-sm-offset-3">
+            <input type="text" placeholder="Subject" name="subject" onChange={(event) => this.registerInput(event)} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-6 col-sm-offset-3">
+            <textarea type="text" rows="8" placeholder="Message" name="message" onChange={(event) => this.registerInput(event)} />
+          </div>
+        </div>
+        <div className="row button-row">
+          <div className="col-xs-12 col-sm-6 col-sm-offset-3">
+            <button onClick={this.sendEmail}>Send</button>
+          </div>
         </div>
       </div>
     );
