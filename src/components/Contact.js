ea7e718cc6d71e102dd960d76e62326b8523f3af
April Ablon <ablonapril@gmail.com>
2017-08-26 14:24:38 -0700

    Add another div to wrap p, a, img components in row div for horizontal centering

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 import '../styles/Contact.css';
-import aws from'aws-sdk';
+// import aws from'aws-sdk';
 
 // not going to work on the frontend 
 // need to create back end
-aws.config.loadFromPath('config.json');
+// aws.config.loadFromPath('config.json');
 
 class Contact extends React.Component {
   constructor(props) {
@@ -27,29 +27,29 @@ class Contact extends React.Component {
     console.log('button click: ', this.state);
     
     // create Nodemailer SES transporter
-    let transporter = nodemailer.createTransport({
-        SES: new aws.SES({
-            apiVersion: '2010-12-01'
-        })
-    });
+    // let transporter = nodemailer.createTransport({
+    //     SES: new aws.SES({
+    //         apiVersion: '2010-12-01'
+    //     })
+    // });
     
-    // send some mail
-    transporter.sendMail({
-        from: 'sender@example.com',
-        to: 'ablonapril@gmail.com',
-        subject: 'Message',
-        text: 'I hope this message gets sent!',
-        ses: { // optional extra arguments for SendRawEmail
-            Tags: [{
-                Name: 'tag name',
-                Value: 'tag value'
-            }]
-        }
-    }, (err, info) => {
-        console.log('did not work');
-        console.log(info.envelope);
-        console.log(info.messageId);
-    });
+    // // send some mail
+    // transporter.sendMail({
+    //     from: 'sender@example.com',
+    //     to: 'ablonapril@gmail.com',
+    //     subject: 'Message',
+    //     text: 'I hope this message gets sent!',
+    //     ses: { // optional extra arguments for SendRawEmail
+    //         Tags: [{
+    //             Name: 'tag name',
+    //             Value: 'tag value'
+    //         }]
+    //     }
+    // }, (err, info) => {
+    //     console.log('did not work');
+    //     console.log(info.envelope);
+    //     console.log(info.messageId);
+    // });
   }
 
   registerInput(event) {
@@ -57,19 +57,21 @@ class Contact extends React.Component {
     updateObj[event.target.name] = event.target.value;
     this.setState(updateObj);
   }
-
+  
   render() {
     return (
       <div id="contact">
         <p className="contact-title">Say Hi 👋</p>
         <div className="row connect-row">
-          <p className="subtitle">Connect: </p>
-          <a target="_blank" href="https://www.linkedin.com/in/aprilablon">
-            <img className="icon" alt="linkedin.com/in/aprilablon" src="/assets/linkedin.svg" />
-          </a>
-          <a target="_blank" href="https://twitter.com/aprilablon">
-            <img className="icon" alt="@aprilablon" src="/assets/twitter.png"/>
-          </a>
+          <div className="social">
+            <p className="subtitle">Connect: </p>
+            <a target="_blank" href="https://www.linkedin.com/in/aprilablon">
+              <img className="icon" alt="linkedin.com/in/aprilablon" src="/assets/linkedin.svg" />
+            </a>
+            <a target="_blank" href="https://twitter.com/aprilablon">
+              <img className="icon" alt="@aprilablon" src="/assets/twitter.png"/>
+            </a>
+          </div>
         </div>
         <div className="row">
           <div className="col-xs-12 col-sm-3 col-sm-offset-3">
