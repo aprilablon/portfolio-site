f78e76668cd2aefe6762575af9855341a7c8d78b
April Ablon <ablonapril@gmail.com>
2017-08-31 17:23:10 -0700

    Change onClick to onMouseEnter and onMouseLeave for toggling menu display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { LinkContainer } from 'react-router-bootstrap';
-// import { Navbar, NavItem, NavbarBrand, Nav } from 'react-bootstrap';
-// import NavbarCollapse from 'react-bootstrap/lib/NavbarCollapse';
-// import NavbarHeader from 'react-bootstrap/lib/NavbarHeader';
-// import NavbarToggle from 'react-bootstrap/lib/NavbarToggle';
+import { CSSTransitionGroup } from 'react-transition-group';
 import '../styles/Header.css';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({
+      menuOpen: !this.state.menuOpen
+    });
+  }
+
   render() {
+    let menu;
+    if (this.state.menuOpen) {
+      menu = <div id="dropdown">
+            <Link to="/Bandwith" className="nav-link drop-link">Bandwith</Link>
+            <Link to="/Fridgr" className="nav-link drop-link">Fridgr</Link>
+            <Link to="/Tribal" className="nav-link drop-link">Tribal</Link>
+            <Link to="/Rocket" className="nav-link drop-link">Rocket Launch Watch</Link>
+        </div>
+    } else {
+      menu = ""
+    }
     return (
       <div className="header">
         <div className="row">
-          <div className="col-xs-12 col-sm-4 small-center brand-text">
+          <div className="col-xs-12 col-sm-4 brand-text">
             <Link to="/" className="link" id="home">April Ablon</Link>
             <div>
               <p id="title">Full Stack Software Engineer</p>
             </div>
           </div>
-          <div className="col-xs-12 col-sm-5 col-sm-offset-3 small-center" id="nav">
-            <Link to="/" className="nav-link link">Projects</Link>
+          <div className="col-xs-12 col-sm-8" id="nav">
+            <div id="menu">
+              <div className="nav-link link" onMouseEnter={this.toggleMenu} onMouseLeave={this.toggleMenu}>Projects</div>
+            </div>
             <Link to="/about" className="nav-link link">About</Link>
             <Link to="/contact" className="nav-link link">Contact</Link>
+            <CSSTransitionGroup transitionName="menu" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}> 
+              {menu} 
+            </CSSTransitionGroup>
           </div>
         </div>
       </div>
@@ -30,27 +56,3 @@ class Header extends React.Component {
 }
 
 export default Header;
-
-/* <Navbar inverse collapseOnSelect>
-  <NavbarHeader>
-    <NavbarBrand>
-      <LinkContainer to="/">
-        <a>April Ablon</a>
-      </LinkContainer>
-    </NavbarBrand>
-    <NavbarToggle />
-  </NavbarHeader>
-  <NavbarCollapse>
-    <Nav pullRight>
-      <LinkContainer to="/">
-        <NavItem eventKey={1}>Projects</NavItem>
-      </LinkContainer>
-      <LinkContainer to="/about">
-        <NavItem eventKey={2}>About</NavItem>
-      </LinkContainer>
-      <LinkContainer to="/contact">
-        <NavItem eventKey={2}>Contact</NavItem>
-      </LinkContainer>
-    </Nav>
-  </NavbarCollapse>
-</Navbar> */
